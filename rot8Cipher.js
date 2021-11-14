5b9eb9253d872efba1656b1869730223d45a9814
Aliaksandr Filitaryn <aliaksandr.filitaryn@awtg.co.uk>
2021-11-14 17:13:40 +0300

    revert mistaken commit

--- rot8Cipher.js
+++
@@ -1,6 +0,0 @@
-const { SHIFT_STEP_ROT8 } = require('./cipherConstants');
-const { getShiftedEncrypted } = require('./cipherWithShift');
-
-const getRot8Encrypted = (string, decipher) => getShiftedEncrypted(string, SHIFT_STEP_ROT8, decipher);
-
-module.exports = { getRot8Encrypted };
